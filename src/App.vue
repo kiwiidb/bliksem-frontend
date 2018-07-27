78edc951131b3e5e1278b716f9098dbf26100c4a
kdebacker <kwinten.debacker@robovision.eu>
2018-07-27 14:23:55 +0200

    better layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
-<div class="b">
-<button v-on:click="getInvoice">Pay</button>
-<div >{{ invoice }}</div>
-<qrcode-vue :value=invoice></qrcode-vue>
+<div>
+<v-app>
+<body >
+<h1 align="center" id="header">Give me some money, thanks.</h1>
+  <div align="center">
+<v-btn @click="getInvoice($event, '1000')">
+1000 sats
+</v-btn>
+<v-btn @click="getInvoice($event, '10000')">
+10000 sats
+</v-btn>
+<v-btn @click="getInvoice($event, '100000')">
+100000 sats
+</v-btn>
+  </div>
+<div id="wrapper" >
+<div id="first" v-show="invoice != ''">Invoice for {{ value_invoice }} satoshis: {{ invoice }}</div>
+<qrcode-vue v-show="invoice != ''" id="second" :value=invoice size=150></qrcode-vue>
+</div>
+</body>
+</v-app>
 </div>
 </template>
 
@@ -14,16 +31,17 @@ export default {
   data () {
     return {
       invoice: '',
-      posts: [],
+      value_invoice: '',
       errors: []
     }
   },
   methods: {
-    getInvoice: function (event) {
-      axios.post(`http://localhost:8081/addinvoice`, {body: '"value" : 10'})
+    getInvoice: function (event, amount) {
+      axios.post(`http://localhost:8081/addinvoice`, {body: '"value" : ' + amount})
         .then(response => {
           console.log(response.data.payment_request)
           this.invoice = response.data.payment_request
+          this.value_invoice = amount
         })
         .catch(e => {
           this.errors.push(e)
@@ -48,12 +66,22 @@ export default {
 </script>
 
 <style>
-  div.b {
+
+#wrapper {
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     word-wrap: break-word;
-    max-width: 300px;
+    width: 600px;
+    overflow: hidden; /* will contain if #first is longer than #second */
+}
+#first {
+    width: 300px;
+    margin-right: 80px;
+    float:left; /* add this */
+}
+#second {
+    overflow: hidden; /* if you don't want #second to wrap below #first */
 }
 </style>
